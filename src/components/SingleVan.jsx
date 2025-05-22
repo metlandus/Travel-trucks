fc2b6162ea12876c7a25d6dbe12a79a86ecdc6e5
Mete <metlandus@hotmail.com.tr>
2025-05-22 23:57:35 +0300

    Flex created

--- src/components/SingleVan.jsx
+++ src/components/SingleVan.jsx
@@ -3,10 +3,14 @@ import React from "react";
 const SingleVan = ({ vanName, rating, location, img }) => {
 	return (
 		<div className="flex flex-col gap-8 border-1 rounded-2xl border-gray-light">
-			<img src={img} />
-			{rating}
-			{location}
-			{vanName}
+			<div className="flex">
+				<img src={img} className="rounded-lg flex-1/3" />
+				<div className="flex-2/3">
+					<p>{vanName}</p>
+					{rating}
+					{location}
+				</div>
+			</div>
 		</div>
 	);
 };
