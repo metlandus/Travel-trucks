203363266d163b0fe0401878020100cc2590b537
Mete <metlandus@hotmail.com.tr>
2025-05-26 23:59:42 +0300

    A lot 2

--- src/pages/VanDetailsPage.jsx
+++ src/pages/VanDetailsPage.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { fetchVans } from "../redux/vansSlice";
+import svg from "../assets/icomoon/symbol-defs.svg";
 
 function VanDetailsPage() {
 	const { id } = useParams();
@@ -22,8 +23,36 @@ function VanDetailsPage() {
 	console.log(van);
 	return (
 		<div className="px-16 pt-12">
-			<h1>{van.name}</h1>
-			<p>{van.rating}</p>
+			<div className="flex flex-col gap-2">
+				<p className="text-main font-semibold text-2xl">{van.name}</p>
+				<div className="flex gap-4">
+					<div className="flex items-center gap-1">
+						<svg width={16} height={16} className="fill-rating">
+							<use href={`${svg}#icon-rating`} />
+						</svg>
+						<p className="underline">
+							{van.rating} ({van.reviews.length} Reviews)
+						</p>
+					</div>
+					<div className="flex items-center gap-1">
+						<svg width={16} height={16}>
+							<use href={`${svg}#icon-map`} />
+						</svg>
+						<p>{van.location}</p>
+					</div>
+				</div>
+				<p className="font-main font-semibold text-2xl">
+					€{van.price}.00
+				</p>
+			</div>
+			<div className="flex justify-between">
+				{van.gallery.map((image) => (
+					<img
+						src={image.original}
+						className="rounded-lg"
+					></img>
+				))}
+			</div>
 		</div>
 	);
 }
